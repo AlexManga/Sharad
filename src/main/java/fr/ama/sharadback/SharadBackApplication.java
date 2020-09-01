b27a844ceabeae30a19e2e0427065ae71180b56b
Alexis <Alexis267@hotmail.com>
2020-09-01 22:01:39 +0200

    Refactor archi projet

---
+++ src/main/java/fr/ama/sharadback/SharadBackApplication.java
@@ -0,0 +1,13 @@
+package fr.ama.sharadback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharadBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharadBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ama/sharadback/SharadBackApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ama.sharadback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharadBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
