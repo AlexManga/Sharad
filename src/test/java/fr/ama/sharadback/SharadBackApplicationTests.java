caf8be6405fff747a55c550822034707cd3df940
Alexandre Maille <an email>
2021-01-06 23:02:38 +0100

    Premier TU du controller

--- src/test/java/fr/ama/sharadback/SharadBackApplicationTests.java
+++ src/test/java/fr/ama/sharadback/SharadBackApplicationTests.java
@@ -1,13 +1,25 @@
 package fr.ama.sharadback;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class SharadBackApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		mockMvc.perform(get("/api/hello"))
+				.andExpect(MockMvcResultMatchers.content().string("Hello Alex 😀"));
 	}
 
 }
