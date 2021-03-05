aefa8fd80aba2338c98b726b2a81571fb1fefc49
Alexandre Maille <al.maille@laposte.net>
2021-03-05 17:10:38 +0100

    [POST & DELETE /tag][TDD] : POST /tag puis DELETE /tag doit redonner une liste vide si le tag est le même

--- src/test/java/fr/ama/sharadback/TagControllerIntegrationTest.java
+++ src/test/java/fr/ama/sharadback/TagControllerIntegrationTest.java
@@ -3,8 +3,10 @@ package fr.ama.sharadback;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.util.Files.delete;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.io.File;
@@ -17,6 +19,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -87,6 +90,41 @@ public class TagControllerIntegrationTest {
 		assertThat(retrievedTags[0])
 				.usingRecursiveComparison()
 				.isEqualTo(new Tag(storedTagId, arbitraryTagContent));
+	}
+
+	@Test
+	void deleting_a_non_existing_tag_should_give_back_404() throws Exception {
+		mockMvc.perform(delete("/tag/arbitraryNoteId")).andExpect(status().isNotFound());
+	}
+
+	@Test
+	void posting_then_deleting_should_give_back_no_tag() throws Exception {
+		TagContent arbitraryTagContent = new TagContent("arbitrary tag", List.of("elemId1", "elemId2"));
+
+		String postResponseBody = mockMvc.perform(post("/tag").contentType(APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(arbitraryTagContent)))
+				.andExpect(status().is2xxSuccessful())
+				.andReturn()
+				.getResponse().getContentAsString();
+
+		StorageId tagId = objectMapper.readValue(postResponseBody, StorageId.class);
+
+		String getResponseBody = mockMvc.perform(get("/tag"))
+				.andExpect(status().is2xxSuccessful())
+				.andReturn().getResponse().getContentAsString();
+
+		Tag[] retrievedTags = objectMapper.readerFor(Tag[].class).readValue(getResponseBody);
+		assertThat(retrievedTags).hasSize(1);
+		assertThat(retrievedTags[0])
+				.usingRecursiveComparison()
+				.isEqualTo(new Tag(tagId, arbitraryTagContent));
+
+		mockMvc.perform(MockMvcRequestBuilders.delete("/tag/" + tagId.getId() + "/elemId1,elemId2"))
+				.andExpect(status().is2xxSuccessful());
+
+		mockMvc.perform(get("/tag"))
+				.andExpect(status().is2xxSuccessful())
+				.andExpect(content().string("[]"));
 	}
 
 }
